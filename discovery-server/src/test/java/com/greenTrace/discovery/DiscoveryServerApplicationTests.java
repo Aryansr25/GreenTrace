54bbddcc3a79cd16a4c9fbccf32e91aefdb7fb1b
Aryansr25 <143589959+Aryansr25@users.noreply.github.com>
2026-05-14 21:25:04 +0530

    discovery-server

---
+++ discovery-server/src/test/java/com/greenTrace/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenTrace.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
